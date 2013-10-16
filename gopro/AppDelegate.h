d4a7ca39b52b65187ba44e460cada420882dfac1
xiaohujin <xiaohujin@xiaohujintekiMac.local>
2013-10-16 10:20:51 +0800

    Initial Commit

---
+++ gopro/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gopro
+//
+//  Created by xiaohujin on 13-10-16.
+//  Copyright (c) 2013年 xiaohujin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
